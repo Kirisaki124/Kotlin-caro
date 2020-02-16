f10341c9aa549993daac6fc39a3ab24fc103ac4f
Kirisaki <kirisaki124@yahoo.com>
2020-02-16 21:33:32 +0700

    Imple: Check win

--- app/src/main/java/com/example/testkotlin/MainActivity.kt
+++ app/src/main/java/com/example/testkotlin/MainActivity.kt
@@ -14,6 +14,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     var gameSize: Int? = 3
     var activePlayer = 1
+    private val winCondition = 3
 //    player 1: X, player 2: O
     private var map = Array(gameSize!!) { i ->
         Array(gameSize!!) { j ->
@@ -51,6 +52,7 @@ class MainActivity : AppCompatActivity() {
                         activePlayer = 1
                     }
                     newCellButton?.isEnabled = false
+                    checkWin(i, j)
                 }
                 newRow.addView(newCellButton)
             }
@@ -77,8 +79,95 @@ class MainActivity : AppCompatActivity() {
             }
         }
         createPlayground()
+//        Toast.makeText(applicationContext, "Size: $gameSize", Toast.LENGTH_LONG).show()
+    }
 
-        Toast.makeText(applicationContext, "Size: $gameSize", Toast.LENGTH_LONG).show()
+    private fun checkWin(x: Int, y: Int) {
+        var winCount = 1
+        var player = map[x][y]
+
+        // Check col ------------------------------
+        winCount = 1
+        // Check col go down
+        for(i in 1 .. winCondition) {
+            if (x + i >= gameSize!!) break
+            if (map[x + i][y] == player) winCount ++
+            else break
+        }
+        // Check col go up
+        for(i in 1 .. winCondition) {
+            if (x - i < 0) break
+            if (map[x - i][y] == player) winCount ++
+
+            else break
+        }
+        // ----------------------------------------
+        if (winCount >= winCondition) {
+            Toast.makeText(this, "Player $player win", Toast.LENGTH_LONG).show()
+            return
+        }
+        // Check row ------------------------------
+        winCount = 1
+        // Check row right
+        for(i in 1 .. winCondition) {
+            if (y + i >= gameSize!!) break
+            if (map[x][y + i] == player) winCount++
+            else break
+        }
+        // Check row left
+        for(i in 1 .. winCondition) {
+            if (y - i < 0) break
+            if (map[x][y - i] == player) winCount++
+            else break
+        }
+        // ----------------------------------------
+        if (winCount >= winCondition) {
+            Toast.makeText(this, "Player $player win", Toast.LENGTH_LONG).show()
+            return
+        }
+        // Check dig left to right ----------------
+        winCount = 1
+        // go down
+        for(i in 1 .. winCondition) {
+            if (y + i >= gameSize!!) break
+            if (x + i >= gameSize!!) break
+            if (map[x + i][y + i] == player) winCount++
+            else break
+        }
+        // go up
+        for(i in 1 .. winCondition) {
+            if (y - i < 0) break
+            if (x - i < 0) break
+            if (map[x - i][y - i] == player) winCount++
+            else break
+        }
+        if (winCount >= winCondition) {
+            Toast.makeText(this, "Player $player win", Toast.LENGTH_LONG).show()
+            return
+        }
+        // ----------------------------------------
+
+        // Check dig right to left ----------------
+        winCount = 1
+        // go down
+        for(i in 1 .. winCondition) {
+            if (y - i < 0) break
+            if (x + i >= gameSize!!) break
+            if (map[x + i][y - i] == player) winCount++
+            else break
+        }
+        // go up
+        for(i in 1 .. winCondition) {
+            if (y + i >= gameSize!!) break
+            if (x - i < 0) break
+            if (map[x - i][y + i] == player) winCount++
+            else break
+        }
+        if (winCount >= winCondition) {
+            Toast.makeText(this, "Player $player win", Toast.LENGTH_LONG).show()
+            return
+        }
+        // ----------------------------------------
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
