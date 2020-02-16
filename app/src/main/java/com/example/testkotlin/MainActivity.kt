d326acf97fb5c7e03b049600b450c707aab5d3cf
Kirisaki <kirisaki124@yahoo.com>
2020-02-16 13:14:41 +0700

    Imple: map map to button

--- app/src/main/java/com/example/testkotlin/MainActivity.kt
+++ app/src/main/java/com/example/testkotlin/MainActivity.kt
@@ -3,10 +3,9 @@ package com.example.testkotlin
 import android.app.Activity
 import android.os.Bundle
 import android.view.MotionEvent
-import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.TableRow
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,6 +13,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     var gameSize: Int? = 3
+    var activePlayer = 1
+//    player 1: X, player 2: O
     private var map = Array(gameSize!!) { i ->
         Array(gameSize!!) { j ->
             "the String at position $i, $j" // provide some initial value based on i and j
@@ -32,33 +33,52 @@ class MainActivity : AppCompatActivity() {
         return super.dispatchTouchEvent(ev)
     }
 
+    private fun createPlayground() {
+        gameScreen.removeAllViews()
+        for(i in 0 until gameSize!!) {
+            val newRow = TableRow(this)
+            for(j in 0 until gameSize!!) {
+
+                var newCellButton = Button(applicationContext)
+                newCellButton.setOnClickListener {
+                    if (activePlayer == 1) {
+                        map[i][j] = "X"
+                        newCellButton.text = "X"
+                        activePlayer = 2
+                    }else if (activePlayer == 2) {
+                        map[i][j] = "O"
+                        newCellButton.text = "O"
+                        activePlayer = 1
+                    }
+                    newCellButton?.isEnabled = false
+                }
+                newRow.addView(newCellButton)
+            }
+            gameScreen.addView(newRow)
+        }
+    }
+
     private fun createGame(){
-        if (!gameSizeInput.text.toString().trim().equals("")) {
+        if (gameSizeInput.text.toString().trim() != "") {
             var size = gameSizeInput.text.toString().toInt()
             if (size >= 3) gameSize = size
         }
+
 //        original
 //        map = Array(gameSize!!) { i ->
 //            Array(gameSize!!) { j ->
 //                "the String at position $i, $j" // provide some initial value based on i and j
 //            }
 //        }
-        map = Array(gameSize!!) { i ->
-            Array(gameSize!!) { j ->
+
+        map = Array(gameSize!!) {
+            Array(gameSize!!) {
                 "0"
             }
         }
+        createPlayground()
 
-        val newRow = TableRow(this)
-        for(i in 0 until gameSize!!) {
-            var newCell = TextView(this)
-            newCell.text = i.toString()
-            newRow.addView(newCell)
-        }
-
-        gameScreen!!.addView(newRow)
         Toast.makeText(applicationContext, "Size: $gameSize", Toast.LENGTH_LONG).show()
-//        gameScreen.addView()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
